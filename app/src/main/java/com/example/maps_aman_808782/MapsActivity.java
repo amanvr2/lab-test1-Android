e87e4030b9c1d9c2a836ef1dbcc3554178df177a
amanvr2 <amanvr2@gmail.com>
2021-06-09 21:35:23 -0400

    updated code and added features

--- app/src/main/java/com/example/maps_aman_808782/MapsActivity.java
+++ app/src/main/java/com/example/maps_aman_808782/MapsActivity.java
@@ -26,6 +26,8 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -51,7 +53,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public static Double homeLong = 0.0;
 
 
-   // Polyline line;
+    Polyline line;
     Polygon shape;
     private static final int POLYGON_SIDES = 4;
     List<Marker> markers = new ArrayList();
@@ -131,68 +133,64 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 MarkerOptions options = null;
 
-                if(count == 1) {
-
-                    float results[] = new float[1];
-                    Location.distanceBetween(homeLat,homeLong,latLng.latitude,latLng.longitude,results);
-
-                    options = new MarkerOptions().position(latLng)
-                            .title("A").draggable(true).snippet("Distance from Source is "+ String.valueOf(results[0]));
-                    markers.add(mMap.addMarker(options));
-
-                }
-
-                else if(count == 2) {
-                    float results[] = new float[1];
-                    Location.distanceBetween(homeLat,homeLong,latLng.latitude,latLng.longitude,results);
-
-                     options = new MarkerOptions().position(latLng)
-                            .title("B").draggable(true).snippet("Distance from Source is "+String.valueOf(results[0]));
-                    markers.add(mMap.addMarker(options));
-                }
-
-
-                else if(count == 3) {
-
-                    float results[] = new float[1];
-                    Location.distanceBetween(homeLat,homeLong,latLng.latitude,latLng.longitude,results);
-
-                    options = new MarkerOptions().position(latLng)
-                            .title("C").draggable(true).snippet("Distance from Source is "+String.valueOf(results[0]));
-                    markers.add(mMap.addMarker(options));
-                }
-
-
-                else if(count == 4) {
-                    float results[] = new float[1];
-                    Location.distanceBetween(homeLat,homeLong,latLng.latitude,latLng.longitude,results);
-
-                    options = new MarkerOptions().position(latLng)
-                            .title("D").draggable(true).snippet("Distance from Source is "+String.valueOf(results[0]));
-                    markers.add(mMap.addMarker(options));
-                }
-                else if(count > 4) {
+                if(count > 4){
                     count = 0;
                     clearMap();
                 }
-                /*if (destMarker != null) clearMap();
 
-                destMarker = mMap.addMarker(options);
+                else {
 
-                drawLine();*/
+                    if (count == 1) {
 
-                // check if there are already the same number of markers, we clear the map.
+                        float results[] = new float[1];
+                        Location.distanceBetween(homeLat, homeLong, latLng.latitude, latLng.longitude, results);
+
+                        options = new MarkerOptions().position(latLng)
+                                .title("A").draggable(true).snippet("Distance from Source is " + String.valueOf(results[0]));
+                        markers.add(mMap.addMarker(options));
+
+                    } else if (count == 2) {
+                        float results[] = new float[1];
+                        Location.distanceBetween(homeLat, homeLong, latLng.latitude, latLng.longitude, results);
+
+                        options = new MarkerOptions().position(latLng)
+                                .title("B").draggable(true).snippet("Distance from Source is " + String.valueOf(results[0]));
+                        markers.add(mMap.addMarker(options));
+                    } else if (count == 3) {
+
+                        float results[] = new float[1];
+                        Location.distanceBetween(homeLat, homeLong, latLng.latitude, latLng.longitude, results);
+
+                        options = new MarkerOptions().position(latLng)
+                                .title("C").draggable(true).snippet("Distance from Source is " + String.valueOf(results[0]));
+                        markers.add(mMap.addMarker(options));
+                    } else if (count == 4) {
+                        float results[] = new float[1];
+                        Location.distanceBetween(homeLat, homeLong, latLng.latitude, latLng.longitude, results);
+
+                        options = new MarkerOptions().position(latLng)
+                                .title("D").draggable(true).snippet("Distance from Source is " + String.valueOf(results[0]));
+                        markers.add(mMap.addMarker(options));
+                    }
+
+//                if (destMarker != null) clearMap();
+//
+//                destMarker = mMap.addMarker(options);
+//
+//                drawLine();
+
+                    // check if there are already the same number of markers, we clear the map.
 //                if (markers.size() == POLYGON_SIDES)
 //                    clearMap();
 
 
+                    if (markers.size() == POLYGON_SIDES)
+                        drawShape();
 
 
-                if (markers.size() == POLYGON_SIDES)
-                    drawShape();
 
-
-                markers.get(0).getPosition();
+                    markers.get(0).getPosition();
+                }
             }
 
             private void drawShape() {
@@ -211,6 +209,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             }
 
+
+//            private void drawLine() {
+//                PolylineOptions options = new PolylineOptions()
+//                        .color(Color.BLACK)
+//                        .width(10)
+//                        .add(homeMarker.getPosition(), destMarker.getPosition());
+//                line = mMap.addPolyline(options);
+//            }
+
             private void clearMap() {
 
                 /*if (destMarker != null) {
@@ -227,14 +234,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 shape.remove();
                 shape = null;
             }
-            /*
-            private void drawLine() {
-                PolylineOptions options = new PolylineOptions()
-                        .color(Color.BLACK)
-                        .width(10)
-                        .add(homeMarker.getPosition(), destMarker.getPosition());
-                line = mMap.addPolyline(options);
-            }*/
+
+
         });
 
 //        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
@@ -267,6 +268,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             }
         });
+
+//        mMap.setOnPolylineClickListener(new GoogleMap.OnPolylineClickListener() {
+//            @Override
+//            public void onPolylineClick(@NonNull Polyline polyline) {
+//                Toast.makeText(getApplicationContext(),"line click : ",Toast.LENGTH_LONG).show();
+//            }
+//        });
     }
 
     private void startUpdateLocation() {
@@ -337,7 +345,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             e.printStackTrace();
         }
 
-//                addressText.setText("Address: " + address);
+
         Toast.makeText(getApplicationContext(),"Address : "+address,Toast.LENGTH_LONG).show();
 
     }
@@ -357,7 +365,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Location.distanceBetween(locations.get(3).latitude,locations.get(3).longitude,locations.get(0).latitude,locations.get(0).longitude,results4);
 
         total = (results1[0]+results2[0]+results3[0]+results4[0])/1000;
-//
+
         Toast.makeText(getApplicationContext(),"Total Distance : "+String.valueOf(total)+" Kms",Toast.LENGTH_LONG).show();
 
     }
